3b35da18449253df5fc49da65542e5fcfc3c0ba8
Elijah Luckey <luckeyelijah112@gmail.com>
2020-10-12 18:02:39 -0400

    update driver code

--- src/main.py
+++ src/main.py
@@ -9,14 +9,21 @@ assert sys.version_info >= (3, 6, 0), "Python version too low."
 
 if __name__ == "__main__":
     # the plaintext
-    t = TextResource(
-        "https://www.gutenberg.org/files/100/100-0.txt", online_resource=True)
+    t = TextResource("example_literature/shakespeare.txt")
 
     # key
-    k = "WORDS"
+    k = "AB"
 
     # the ciphertext: v.encrypt()
-    v = VigenereCipher(k, plaintext=t.text)
+    v = VigenereCipher(k, plaintext=t.text, )
 
     # cracking the cipher
     c = VigenereCracker(cipher_text=v.encrypt())
+
+    # set up the
+    assumed_key_len = 2
+    maps = c.crack_one_length(assumed_key_len)
+
+    for i in range(assumed_key_len):
+        analyzer = CharacterFrequencyAnalyzer(map=maps[i])
+        analyzer.print_frequency()
